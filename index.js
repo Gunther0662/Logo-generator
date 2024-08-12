0a89f49362e3be037c64e7c34f461e1659f71816
Gunther0662 <nicholasdiaz17@yahoo.com>
2024-08-12 18:29:13 -0500

    added the inquirer inputs

---
+++ index.js
@@ -0,0 +1,49 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { Shapes } = require('./lib/shapes');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'Logo name',
+        message: 'What is the name of your Logo?', 
+    },
+
+    {
+        type: 'input',
+        name: 'text color',
+        message: 'What color in the text of your Logo?',
+    },
+
+    {
+        type: 'list',
+        message: 'What shape do you want your Logo?',
+        name: 'shape',
+        choices: ['Circle', 'triangle', 'square'],
+    },
+
+    {
+        type: 'input',
+        name: 'shapecolor',
+        message: 'What is the color of the shape of your Logo?',
+    },
+];
+
+class SVG {
+    constructor() {
+        this.name = '',
+        this.shape = '',
+    }
+render() {
+    <svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">${shape} ${name}</svg>
+}
+setText(name) {
+    this.name = <text x="100" y="100" font-weight="bold" text-anchor="middle">${name}${color}</text>
+} 
+setShape(shape) {
+    this.shape = shape.render
+}
+}
+function init() {
+    inquirer
+}
