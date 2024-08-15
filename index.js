2176b0c3b8e53a4a761bbf428717a8ad651e2558
Gunther0662 <nicholasdiaz17@yahoo.com>
2024-08-15 18:29:28 -0500

    shape problem

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const {writeFile} = require('fs/promises');
 const {Circle, Square, Triangle} = require('./lib/shapes');
 
-class Cli {
+class CLI {
     run(){
         return inquirer.prompt([    
         {
@@ -30,8 +30,25 @@ class Cli {
             message: 'What is the color of the shape of your Logo?',
         },
     ])
-    .then (({logo, name, shape, shapecolor }) => {
-        
+
+    .then (({logo, name, shape, shapecolor}) => {
+        let shapeChoice;
+        switch(shape) {
+            case 'Circle':
+                shapeChoice = new Circle()
+                break;
+            case 'Square':
+                    shapeChoice = new Square()
+                    break;
+            default:
+                    shapeChoice = new Triangle()
+                    break;
+        }
+shapeChoice.addcolor(shapecolor)
+const svg = new SVG()
+svg.setText(logo, name);
+svg.setShape(shape)
+return writeFile("Logo.svg", svg.render())
     })
     }
 }
@@ -42,14 +59,16 @@ class SVG {
     }
 
     render() {
-        `<svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">${this.shapeElement} ${this.textElement}</svg>`
+       return `<svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">${this.shapeElement} ${this.textElement}</svg>`
     };
 
     setText(text, color) {
-        this.textElemnet = `<text x="100" y="100" font-weight="bold" text-anchor="middle">${text}${color}</text>`
+        this.textElement = `<text x="100" y="100" font-weight="bold" text-anchor="middle">${text}${color}</text>`
     };
 
     setShape(shape) {
         this.shapeElement = shape.render()
     };
 };
+new CLI().run()
+module.exports={SVG,CLI}
